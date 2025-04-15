227ce3539fa62e6fe4515e8d87de8a44374bdd11
parintorn0 <github@alias.toparin.me>
2025-04-16 02:01:36 +0700

    revert steam to stable package

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -77,7 +77,6 @@ in {
   # Steam
   programs.steam = {
     enable = true;
-    package = unstablePkgs.steam;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
